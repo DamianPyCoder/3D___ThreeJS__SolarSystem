85a68d6f439aa2e6d5841522e6fe96e78e16f24c
Damian R. Molin <119323521+DamianPyCoder@users.noreply.github.com>
2023-05-17 20:59:08 +0200

    Add files via upload

---
+++ Code_solar_system_threeJS/src/MainContainer.jsx
@@ -0,0 +1,37 @@
+import { useHelper } from '@react-three/drei'
+import AnimatedStars from './AnimatedStars'
+import { useRef } from 'react'
+
+import * as THREE from 'three'
+
+import Earth from './scenes/earth/Earth'
+
+const MainContainer = () => {
+  const directionalLightRef = useRef()
+  const directionalLightRefTwo = useRef()
+  useHelper(directionalLightRef, THREE.DirectionalLightHelper, 1, 'hotpink')
+  useHelper(directionalLightRefTwo, THREE.DirectionalLightHelper, 1, 'hotpink')
+  return (
+    <>
+      <color attach='background' args={['black']} />
+      <AnimatedStars />
+      <directionalLight
+        castShadow
+        ref={directionalLightRef}
+        position={[0, 0, 10]}
+        intensity={1}
+        // color={0xff0000}
+      />
+      <directionalLight
+        castShadow
+        ref={directionalLightRefTwo}
+        position={[0, 0, -10]}
+      />
+      {/* <ambientLight /> */}
+
+      <Earth displacementScale={0.15} />
+    </>
+  )
+}
+
+export default MainContainer
